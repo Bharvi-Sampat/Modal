34a3080e090f4bf412649152e7bf528d520d4437
Bharvi-Sampat <bharvi.sampat@gmail.com>
2022-05-21 23:43:22 +0530

    Modal window Open and CLose added

--- README.md
+++
@@ -1,5 +0,0 @@
-# **_Guess my Number_**
-
-## _A number guessing game created using HTML, CSS and JavaScript_
-
-[Click here to play The Game]()
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.open-modal');
+
+const openModal = function() {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+}
+
+const closeModal = function() {
+    console.log('Close button clicked');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    
+btnsOpenModal[i].addEventListener('click', openModal );
+}
+
+btnsCloseModal.addEventListener('click', closeModal);;
+overlay.addEventListener('click', closeModal);
+
