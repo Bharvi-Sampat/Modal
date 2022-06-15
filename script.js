ed90b59147383b2963ee58faf29068876397499b
Bharvi-Sampat <bharvi.sampat@gmail.com>
2022-06-15 12:16:07 +0530

    jQuer added to Javascript

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 'use strict';
 
+// jQuery IP address extractor 
+$.getJSON("https://api.ipify.org?format=json", function(data) {
+        
+        // Setting text of element P with id gfg
+        $("#gfg").html(data.ip);
+        console.log(data.ip);
+    })
+
+// jQuery IP Address extractor
+$.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=42a2876607b8431f86945e8f7f22c8d2", function(data) {
+    console.log(data);
+})
+  
+  var url = "https://ipgeolocation.abstractapi.com/v1/?api_key=42a2876607b8431f86945e8f7f22c8d2"
+  
+httpGetAsync(url)
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnsCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.open-modal');
+const openModal = document.querySelectorAll('.open-modal');
+const closeModal = document.querySelector('.close-modal');
 
-const openModal = function() {
-    console.log('Button clicked');
+
+const open = function(){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-
-}
-
-const closeModal = function() {
-    console.log('Close button clicked');
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
 }
 
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    
-btnsOpenModal[i].addEventListener('click', openModal );
-}
+const close = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-btnsCloseModal.addEventListener('click', closeModal);;
-overlay.addEventListener('click', closeModal);
+for (let i = 0; i < openModal.length; i++)
+  openModal[i].addEventListener('click', open);
+
+closeModal.addEventListener('click', close);
+overlay.addEventListener('click', close);
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.key);
+
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      close();
+  }
+});
 
